fa14f11454f895b6aabea15ea8fcd654e3460792
gary <gary.white@lodgit.com>
2021-12-02 13:37:27 +0100

    code working

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function assertEquals (strng) {
-    return strng;
+function assertEquals (str) {
+    let newString = "";
+    for(let i = 0; i < str.length; i++){
+        if (str[i] == str[i].toUpperCase()){
+            newString = newString + str[i].toLowerCase();
+        } else if (str[i] == str[i].toLowerCase()){
+            newString = newString + str[i].toUpperCase();
+        }
+    }
+    return newString;
 }
 
+//To pass the codewars kata you must change to 'this':
+// String.prototype.toAlternatingCase = function () {
+//     let newString = "";
+//       for(let i = 0; i < this.length; i++){
+//           if (this[i] == this[i].toUpperCase()){
+//               newString = newString + this[i].toLowerCase();
+//           } else if (this[i] == this[i].toLowerCase()){
+//               newString = newString + this[i].toUpperCase();
+//           }
+//       }
+//       return newString;
+//   }
+  
 console.log(assertEquals("hello world")); // Return "HELLO WORLD"
 console.log(assertEquals("HELLO WORLD")); // Return "hello world"
 console.log(assertEquals("hello WORLD")); // Return "HELLO world"
